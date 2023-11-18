6e4b8e048e6baddc714d5aa0972c126d16d455d1
Mr-BAKH <mr.bakh.usb@gmail.com>
2023-11-18 16:43:48 +0330

    original with router and tailwind

---
+++ tailwind.config.js
@@ -0,0 +1,7 @@
+module.exports = {
+  content: ["./app/**/*.{js,ts,tsx,jsx}","./app/screens/**/*.{js,ts,tsx,jsx}","./app/components/**/*.{js,ts,tsx,jsx}", "./index.js"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
